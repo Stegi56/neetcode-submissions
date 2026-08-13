87df773134377174c4bff7e2da7cc3cfaa2c02a7
Joel Staugaitis <91616550+Stegi56@users.noreply.github.com>
2026-08-13 23:09:33 +0100

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.py
@@ -0,0 +1,35 @@
+class TreeNode:
+    def __init__(self):
+        self.word = False
+        self.leaves = {}
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TreeNode()
+        #track each layer
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for char in word:
+            if char not in cur.leaves.keys():
+                cur.leaves[char] = TreeNode()
+            cur = cur.leaves[char]
+        cur.word = True
+
+    def search(self, word: str) -> bool:
+        def dfs(i, node):
+            for i in range(i, len(word)):
+                c = word[i]
+
+                if c == ".":
+                    for val in node.leaves.values():
+                        if dfs(i + 1, val):
+                            return True
+                    return False
+                else:
+                    if c not in node.leaves.keys():
+                        return False
+                    node = node.leaves[c]
+            return node.word
+
+        return dfs(0, self.root)
