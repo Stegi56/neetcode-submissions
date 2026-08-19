4dc4fd540703ff50a448fd7ea6103cbfee7a2f01
Joel Staugaitis <91616550+Stegi56@users.noreply.github.com>
2026-08-19 19:36:19 +0100

    Add: valid-tree - submission-13

---
+++ Data Structures & Algorithms/valid-tree/submission-13.py
@@ -0,0 +1,25 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if not edges:
+            return True
+        
+        nodes = defaultdict(list)
+        for edge in edges:
+            nodes[edge[0]].append(edge[1])
+            nodes[edge[1]].append(edge[0])
+
+        visited = set()
+        def checkNoLoops(node, prev):
+            if node in visited:
+                return False
+
+            visited.add(node)
+            valid = True
+            for leaf in nodes[node]:
+                if leaf != prev:
+                    valid = valid and checkNoLoops(leaf, node)
+                if not valid:
+                    break
+            return valid
+
+        return checkNoLoops(edges[0][0], None) and len(visited) == n
