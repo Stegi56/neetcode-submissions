0a2084867b38817e069ef521a72014d2b45d13a2
Joel Staugaitis <91616550+Stegi56@users.noreply.github.com>
2026-08-21 23:13:59 +0100

    Add: find-target-in-rotated-sorted-array - submission-6

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-6.py
@@ -0,0 +1,32 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        #find rotation point
+        #treat before/ after point as sepperate arrays
+        # search in the right one based on if range is <> than target
+
+        l, r = 0, len(nums) - 1
+
+        while l < r:
+            cur = (l + r) // 2
+            if nums[cur] > nums[r]:
+                l = cur + 1
+            else:
+                r = cur
+
+        splitStart = l
+
+        if splitStart == 0 or target <= nums[-1]:
+            l, r = splitStart, len(nums) - 1
+        else:
+            l, r = 0, splitStart - 1
+
+        while l <= r:
+            cur = (l + r) // 2
+            if nums[cur] == target:
+                return cur
+            elif nums[cur] < target:
+                l = cur + 1
+            else:
+                r = cur - 1
+
+        return -1
